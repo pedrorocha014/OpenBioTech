89d8084f47b50c178de635620e7578a26746e180
Pedro Almeida Rocha <19pedrorocha96@gmail.com>
2022-09-15 14:47:30 -0300

    wip: create models

---
+++ AnalysisRegister/Models/AnalysisResult.cs
@@ -0,0 +1,15 @@
+﻿namespace AnalysisRegister.Models
+{
+    public class AnalysisResult
+    {
+        public List<Result> Result { get; set; }
+    }
+
+    public class Result
+    {
+        public string Operation { get; set; }
+        public string Message { get; set; }
+        public bool IsSuccess { get; set; }
+        public string Value { get; set; }
+    }
+}
