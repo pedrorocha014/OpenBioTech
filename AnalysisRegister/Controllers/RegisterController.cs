1948b2b41c9a9d50f56bf2d545a72459dca6dd8f
Pedro Almeida Rocha <19pedrorocha96@gmail.com>
2022-09-15 14:48:38 -0300

    feat: add getResults and registerAnalysisResults features

--- AnalysisRegister/Controllers/RegisterController.cs
+++ AnalysisRegister/Controllers/RegisterController.cs
@@ -1,3 +1,5 @@
+using AnalysisRegister.Models;
+using AnalysisRegister.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AnalysisRegister.Controllers
@@ -6,6 +8,29 @@ namespace AnalysisRegister.Controllers
     [Route("[controller]")]
     public class RegisterController : ControllerBase
     {
+        private readonly IRegister _service;
 
+        public RegisterController(IRegister service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterAnalysisResult([FromBody] AnalysisResult analysisResults)
+        {
+            analysisResults.Result.ForEach(result =>
+            {
+                _service.RegisterAnalysisResult(result);
+            });
+
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetResults()
+        {
+            var results = _service.GetResults();
+            return Ok(results);
+        }
     }
 }
