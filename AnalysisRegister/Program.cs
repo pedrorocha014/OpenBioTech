e1569f1efb536e644a911490999d30fd81d4a961
Pedro Almeida Rocha <19pedrorocha96@gmail.com>
2022-09-15 14:46:50 -0300

    wip: create Register API

--- AnalysisRegister/Program.cs
+++ AnalysisRegister/Program.cs
@@ -1,6 +1,15 @@
+using AnalysisRegister.DataBase;
+using AnalysisRegister.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<RegisterDbContext>(options =>
+            options.UseNpgsql("Host=localhost;Database=analysisDb;Port=5432;Username=admin;Password=123456"));
+
+builder.Services.AddScoped<IRegister, Register>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
