c7bb6681277161b87b390a148f8a824c180a24e7
Pedro Almeida Rocha <19pedrorocha96@gmail.com>
2022-09-16 15:05:32 -0300

    feat: create react app

---
+++ analysis-app/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import AnalysisForms from './components/analysisForms/AnalysisForms';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <AnalysisForms/>
+    </div>
+  );
+}
+
+export default App;
