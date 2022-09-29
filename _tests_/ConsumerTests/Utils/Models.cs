118f787bd9e8eda2aabe135c515eecfa4eee3a8d
Pedro Almeida Rocha <19pedrorocha96@gmail.com>
2022-09-29 16:07:36 -0300

    wip: create Models utils to consumer

---
+++ _tests_/ConsumerTests/Utils/Models.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumerTests.Utils
+{
+    public class Models
+    {
+        public List<char> sequence { get => GetSequence(); }
+
+        private List<char> GetSequence()
+        {
+            return new List<char> { 'M', 'F', 'V' };
+        }
+    }
+}
