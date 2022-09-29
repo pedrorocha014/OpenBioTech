e25a570a55ac3f5d7df05cc2f1f1154104d06d4f
Pedro Almeida Rocha <19pedrorocha96@gmail.com>
2022-09-29 16:07:52 -0300

    wip: create Analysis consumer tests

---
+++ _tests_/ConsumerTests/Analysis/DeleteOperationTest.cs
@@ -0,0 +1,51 @@
+﻿using AnalysisConsumer.Analysis;
+using ConsumerTests.Utils;
+using System.Text.Json;
+
+namespace ConsumerTests.Analysis
+{
+    public class DeleteOperationTest
+    {
+        Models models = new Models();
+
+        [Test]
+        public void Execution_Should_Not_Delete_When_Others_Operations()
+        {
+            var sequence = models.sequence;
+            var mutations = new List<string> { "2insAD" };
+            var deleteOperation = new DeleteOperation(sequence, mutations);
+
+            var result = deleteOperation.ExecuteOperation().Value;
+            var resultObj = JsonSerializer.Deserialize<List<char>>(result);
+
+            Assert.That(sequence, Is.EqualTo(resultObj));
+        }
+
+        [Test]
+        public void Execution_Should_Delete_One_Protein()
+        {
+            var sequence = models.sequence;
+            var mutations = new List<string> { "del1" };
+            var deleteOperation = new DeleteOperation(sequence, mutations);
+
+            var result = deleteOperation.ExecuteOperation().Value;
+            var resultObj = JsonSerializer.Deserialize<List<char>>(result);
+
+            Assert.That(resultObj[0], Is.EqualTo('-'));
+        }
+
+        [Test]
+        public void Execution_Should_Delete_Range_Protein()
+        {
+            var sequence = models.sequence;
+            var mutations = new List<string> { "del2-3" };
+            var deleteOperation = new DeleteOperation(sequence, mutations);
+
+            var result = deleteOperation.ExecuteOperation().Value;
+            var resultObj = JsonSerializer.Deserialize<List<char>>(result);
+
+            Assert.That(resultObj[1], Is.EqualTo('-'));
+            Assert.That(resultObj[2], Is.EqualTo('-'));
+        }
+    }
+}
---
+++ _tests_/ConsumerTests/Analysis/ReplaceOperationTest.cs
@@ -0,0 +1,49 @@
+﻿using AnalysisConsumer.Analysis;
+using ConsumerTests.Utils;
+using System.Text.Json;
+
+namespace ConsumerTests.Analysis
+{
+    public class ReplaceOperationTest
+    {
+        Models models = new Models();
+
+        [Test]
+        public void Execution_Should_Not_Replace_When_Others_Operations()
+        {
+            var sequence = models.sequence;
+            var mutations = new List<string> { "del1", "ins" };
+            var deleteOperation = new ReplaceOperation(sequence, mutations);
+
+            var result = deleteOperation.ExecuteOperation().Value;
+            var resultObj = JsonSerializer.Deserialize<List<char>>(result);
+
+            Assert.That(sequence, Is.EqualTo(resultObj));
+        }
+
+        [Test]
+        public void Execution_IsNot_Success_When_ProteinPosition_Is_Wrong()
+        {
+            var sequence = models.sequence;
+            var mutations = new List<string> { "A1A" };
+            var deleteOperation = new ReplaceOperation(sequence, mutations);
+
+            var result = deleteOperation.ExecuteOperation();
+
+            Assert.That(result.IsSuccess, Is.False);
+        }
+
+        [Test]
+        public void Execution_Should_Replace_Protein()
+        {
+            var sequence = models.sequence;
+            var mutations = new List<string> { "M1A" };
+            var deleteOperation = new ReplaceOperation(sequence, mutations);
+
+            var result = deleteOperation.ExecuteOperation().Value;
+            var resultObj = JsonSerializer.Deserialize<List<char>>(result);
+
+            Assert.That(resultObj[0], Is.EqualTo('A'));
+        }
+    }
+}
