b1bf096d858e5487613a84b557475b4ecf73b40b
Pedro Almeida Rocha <19pedrorocha96@gmail.com>
2022-11-12 16:29:55 -0300

    feat: add custom error

---
+++ src/OBioTech/Helpers/CustomErrors/ValuePositionExeption.cs
@@ -0,0 +1,17 @@
+﻿namespace OBioTech.Helpers.CustomErrors
+{
+    [Serializable]
+    public class ValuePositionExeption: Exception
+    {
+        public ValuePositionExeption()
+        { }
+
+        public ValuePositionExeption(string message)
+            : base(message)
+        { }
+
+        public ValuePositionExeption(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}
