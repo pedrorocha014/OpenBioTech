9487d0888ec276851e6421df35b298dec32f968e
Pedro Almeida Rocha <19pedrorocha96@gmail.com>
2022-11-12 10:27:19 -0300

    wip

---
+++ src/OBioTech/Controllers/AnalysisController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OBioTech.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AnalysisController : ControllerBase
+    {
+        public AnalysisController()
+        {
+
+        }
+
+        [HttpPost("proteinSequence")]
+        public IActionResult SendProteinSequenceDataToAnalysis()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult Check()
+        {
+            return Ok();
+        }
+    }
+}
