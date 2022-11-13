5ef2f60284247f7aa8816e5eaf4dbc57ca11c4f6
Pedro Almeida Rocha <19pedrorocha96@gmail.com>
2022-11-13 11:55:17 -0300

    feat: add Register Controller

---
+++ src/OBioTech/Controllers/RegisterController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OBioTech.Services.Analysis;
+using OBioTech.Services.Register;
+
+namespace OBioTech.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RegisterController: ControllerBase
+    {
+        private readonly IRegisterService _registerService;
+
+        public RegisterController(IRegisterService registerService)
+        {
+            _registerService = registerService;
+        }
+
+        [HttpGet]
+        public IActionResult GetRegisterResults()
+        {
+            var results = _registerService.GetResults();
+            return Ok(results);
+        }
+    }
+}
